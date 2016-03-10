4820dd21df8bb179753b693ac2c8c264fd553c16
Mikael Lundström <mikael.f.lundstrom@gmail.com>
2016-03-10 20:32:00 +0100

    Added ng-view and and Routing to Form

--- AngularBootstrapMVC/App/AngularFormsApp.js
+++ AngularBootstrapMVC/App/AngularFormsApp.js
@@ -1,3 +1,27 @@
 ﻿
-var angularFormsApp = angular.module('angularFormsApp', []);
+var angularFormsApp = angular.module('angularFormsApp', ["ngRoute"]);
+
+angularFormsApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "App/Home.html",
+            controller: "HomeController"
+        })
+        .when("/newEmployeeForm", {
+            templateUrl: "App/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .otherwise({
+            redirectTo: "/home"
+        });
+});
+
+angularFormsApp.controller("HomeController",
+    function ($scope, $location) {
+
+        $scope.addNewEmployee = function () {
+            $location.path("/newEmployeeForm");
+        };
+    });
+
 
